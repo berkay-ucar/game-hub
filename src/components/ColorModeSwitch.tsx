22474d4406beab1485c52c575d868f24997dcb5c
Berkay Ucar <berkayucar80@gmail.com>
2024-10-22 12:37:00 +0300

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,11 +6,12 @@ const ColorModeSwitch = () => {
   return (
     <HStack>
       <Switch
+        name="swt"
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 };
