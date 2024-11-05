2188afc9adca315a67a2b24b9746e5eb97a48c71
Berkay Ucar <berkayucar80@gmail.com>
2024-11-05 23:32:22 +0300

    Refactoring - fetch games with React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { FetchResponse } from "../services/api-client";
+
 import platforms from "../data/platforms";
 
 export interface Platform {
@@ -6,6 +9,15 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 26 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
